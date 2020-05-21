50494a0da72f3adfe9fd050bc4d5e9ca7bda104a
AnastasiiaFrants <63759092+AnastasiiaFrants@users.noreply.github.com>
2020-05-21 17:35:51 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+(function(){
+	const header = document.querySelector('.header');
+	window.onscroll = () => {
+		if (window.pageYOffset > 50){
+			header.classList.add('header_active');
+		} else {
+			header.classList.remove('header_active');
+
+		}
+	};
+}());
