40b8584ac17ac88ff1d3c568c678be5794512717
space_pirrate <63759092+AnastasiiaFrants@users.noreply.github.com>
2020-05-24 01:10:27 +0300

    Add files via upload
    adaptive

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,37 @@
 		}
 	};
 }());
+$(function(){
+ $('.header__burger').click(function(){
+		 var list = $('.header__nav');
+		 var active = $('.burger__line');
+		 if (list.css('display')=='none'){
+			 list.show();
+		 }
+		 else {
+			 list.hide();
+		 
+ }
+
+ 
+	    });
+	
+	 
+	});	
+$(function(){
+ $('.search_media').click(function(){
+		 var list = $('.search_form');
+		
+		 if (list.css('display')=='none'){
+			 list.show();
+		 }
+		 else {
+			 list.hide();
+		 
+ }
+
+ 
+	    });
+	
+	 
+	});	
